371895e8867e688cde57adae861fd4db9958d8f7
Florian <florian.sasso@outlook.fr>
2023-02-13 15:46:30 +0100

    Set pages contact/team/index with ok url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'index#index'
+  get '/team', to: 'team#team'
+  get '/contact', to: 'contact#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
