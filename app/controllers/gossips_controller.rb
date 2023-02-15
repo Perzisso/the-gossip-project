bfa5388d9df316876843a548ecd8e794ba447e0a
Florian <florian.sasso@outlook.fr>
2023-02-15 18:59:43 +0100

    Delete/Edit action on GossipsController + Display action button on show page

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -11,22 +11,33 @@ class GossipsController < ApplicationController
     end
 
     def create
-        @gossip_create = Gossip.new('text' => params[:text],
-                            'content' => params[:content])
-
+        @gossip_create = Gossip.new('title' => params[:title],
+                                    'content' => params[:content])
         if @gossip_create.save
-            redirect_to gossips_path
+            redirect_to new_gossips_path
         else
             render :new, status: :unprocessable_entity
         end
     end
 
     def edit
+        @gossip = Gossip.find(params[:id])
     end
 
     def update
+        @gossip = Gossip.find(params[:id])
+        if @gossip.update('title' => params[:title],
+                          'content' => params[:content])
+            redirect_to @gossip
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        @gossip = Gossip.find(params[:id])
+        @gossip.destroy
+
+        redirect_to root_path, status: :see_other
     end
 end
