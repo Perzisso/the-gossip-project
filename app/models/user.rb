9682d367c45a769808e15a58ec7774ee166a1c04
Florian <florian.sasso@outlook.fr>
2023-02-16 14:42:47 +0100

    Bootstrap for dropdowns

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
     has_many :gossips
     has_many :sent_messages, foreign_key: 'sender_id', class_name: "PrivateMessage"
     has_many :received_messages, foreign_key: 'recipient_id', class_name: "PrivateMessage"
-    validates :first_name, presence: true, length: { in: 3..14 }
-    validates :last_name, presence: true, length: { in: 3..14 }
-    validates :description, presence: true, length: { in: 15..100 }
+    validates :first_name, presence: true, length: { minimum: 3 }
+    validates :last_name, presence: true, length: { minimum: 3 }
+    validates :description, presence: true, length: { in: 5..100 }
     validates :email, presence: true, uniqueness: true
     validates :age, presence: true
     has_secure_password
+    validates :password, presence: true, length: { minimum: 4 }
 end
